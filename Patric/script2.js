7e49481ec90129928d7c76b92e41e152582467f4
Patric <pmorevs@gmail.com>
2025-11-03 14:14:37 +0200

    list of countries

--- Patric/script2.js
+++ Patric/script2.js
@@ -1,3 +1,4 @@
+var header=document.querySelector('header');
 const data = null;
 
 const xhr = new XMLHttpRequest();
@@ -5,7 +6,12 @@ xhr.withCredentials = false;
 
 xhr.addEventListener('readystatechange', function () {
 	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+		console.log(`Response Text: ${this.responseText}`);
+		console.log(`Response URL: ${this.responseURL}`);
+		//populateHeader(this.responseText);
+		const jsonData = JSON.parse(this.responseText);
+		console.log(jsonData);
+		populateHeader(jsonData)
 	}
 });
 
@@ -14,3 +20,11 @@ xhr.setRequestHeader('x-rapidapi-key', '046bbc92c4msh89a16971c93dbbfp109b1fjsnb7
 xhr.setRequestHeader('x-rapidapi-host', 'country-state-city-search-rest-api.p.rapidapi.com');
 
 xhr.send(data);
+
+function populateHeader(jsonObj){
+	for(let i = 0; i < jsonObj.length; i++){
+		var myH1 = document.createElement('h1');
+		myH1.textContent = jsonObj[i]['name'];
+		header.appendChild(myH1);
+	}
+}
