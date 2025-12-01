0b6deec4ee4e2f7e3b76e68a500abda36b2ba03f
Patric <pmorevs@gmail.com>
2025-12-01 14:54:17 +0200

    country + city

--- Patric/script2.js
+++ Patric/script2.js
@@ -1,37 +1,42 @@
 // var header=document.querySelector('header');
-var selectCountryList = document.getElementById("city");
+var selectCityList = document.getElementById("city");
 
-const data = null;
+var selectCountryList = document.getElementById("country");
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = false;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(`Response Text: ${this.responseText}`);
-		console.log(`Response URL: ${this.responseURL}`);
-		//populateHeader(this.responseText);
-		const jsonData = JSON.parse(this.responseText);
-		console.log(jsonData);
-		populateHeaderJSON(jsonData)
+function validateCountryForm() {
+	let myCountry = document.forms["countryName"]["myCountry"].value;
+	if (myCountry == "") {
+		myCountry = "LV";
 	}
-});
+	getCities(myCountry);
+}
 
-xhr.open('GET', 'https://country-state-city-search-rest-api.p.rapidapi.com/allcountries');
-xhr.setRequestHeader('x-rapidapi-key', '046bbc92c4msh89a16971c93dbbfp109b1fjsnb76d127802f8');
-xhr.setRequestHeader('x-rapidapi-host', 'country-state-city-search-rest-api.p.rapidapi.com');
+function getCities(countryCODE) {
+	const data = null;
 
-xhr.send(data);
+	const xhr = new XMLHttpRequest();
+	xhr.withCredentials = false;
 
-function populateHeaderJSON(jsonObj){
-	// var myLabel = document.createElement('label');
-	// myLabel.textContent = "Select Country";
-	// header.appendChild(myLabel);
-	// var selectCountryList = document.createElement('select');
-	// header.appendChild(selectCountryList);
-	for(let i = 0; i < jsonObj.length; i++){
+	xhr.addEventListener('readystatechange', function () {
+		if (this.readyState === this.DONE) {
+			// console.log(this.responseText);
+			const jsonData = JSON.parse(this.responseText);
+			// console.log(jsonData);
+			populateHeaderJSON(jsonData)
+		}
+	});
+
+	xhr.open('GET', `https://country-state-city-search-rest-api.p.rapidapi.com/cities-by-countrycode?countrycode=${countryCODE}`);
+	xhr.setRequestHeader('x-rapidapi-key', '046bbc92c4msh89a16971c93dbbfp109b1fjsnb76d127802f8');
+	xhr.setRequestHeader('x-rapidapi-host', 'country-state-city-search-rest-api.p.rapidapi.com');
+
+	xhr.send(data);
+}
+
+function populateHeaderJSON(jsonObj) {
+	for (let i = 0; i < jsonObj.length; i++) {
 		var myOption = document.createElement('option');
 		myOption.textContent = jsonObj[i]['name'];
-		selectCountryList.appendChild(myOption);
+		selectCityList.appendChild(myOption);
 	}
 }
